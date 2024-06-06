91410ac8ce92ccc5848c09a52fa1112ce56d80cf
Iverson Lai <iverson.lai121@gmail.com>
2024-06-05 20:38:37 -0700

    added addNode(), addEdge(), hasNode(), hasEdge(), removeNode(), removeEdge(), and ndoes() to ListGraph.java. updated Main to test for these functions.

--- ListGraph.java
+++ ListGraph.java
@@ -4,31 +4,65 @@ public class ListGraph implements Graph {
     private HashMap<String, LinkedList<String>> nodes = new HashMap<>();
 
     public boolean addNode(String n) {
-	     throw new UnsupportedOperationException();
+	     if (hasNode(n)) {
+             return false;
+         }
+
+         nodes.put(n, new LinkedList<>());
+         return true;
     }
 
     public boolean addEdge(String n1, String n2) {
-	     throw new UnsupportedOperationException();
+	     if (!hasNode(n1)) {
+             nodes.put(n1, new LinkedList<>());
+         }
+         if (!hasNode(n2)) {
+             nodes.put(n2, new LinkedList<>());
+         }
+
+         LinkedList<String> list = nodes.get(n1);
+         if (!list.contains(n2)) {
+             list.add(n2);
+             return true;
+         }
+         return false;
     }
 
     public boolean hasNode(String n) {
-	     throw new UnsupportedOperationException();
+        return nodes.containsKey(n);
     }
 
     public boolean hasEdge(String n1, String n2) {
-	     throw new UnsupportedOperationException();
+        if (!nodes.containsKey(n1)) {
+            return false;
+        }
+        LinkedList<String> list = nodes.get(n1);
+        return list.contains(n2);
     }
 
     public boolean removeNode(String n) {
-	     throw new UnsupportedOperationException();
+        if (hasNode(n)) {
+            nodes.remove(n);
+            return true;
+        }
+        return false;
     }
 
     public boolean removeEdge(String n1, String n2) {
-	     throw new UnsupportedOperationException();
+        if (hasEdge(n1, n2)) {
+            LinkedList<String> list = nodes.get(n1);
+            list.remove(n2);
+            return true;
+        }
+        return false;
     }
 
     public List<String> nodes() {
-	     throw new UnsupportedOperationException();
+	     if (nodes.isEmpty()) {
+             throw new RuntimeException("Graph is empty");
+         }
+
+         return new ArrayList<>(nodes.keySet());
     }
 
     public List<String> succ(String n) {
--- Main.java
+++ Main.java
@@ -1,26 +1,113 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 
     // Run "java -ea Main" to run with assertions enabled (If you run
     // with assertions disabled, the default, then assert statements
     // will not execute!)
-    
-    public static void test1() {
-	Graph g = new ListGraph();
-	assert g.addNode("a");
-	assert g.hasNode("a");
+
+	public static void lg_test_addNode() {
+		Graph g = new ListGraph();
+
+		assert g.addNode("a");
+		assert g.hasNode("a");
+
+		assert !g.hasNode("b");
     }
 
-    public static void test2() {
-	Graph g = new ListGraph();
-	EdgeGraph eg = new EdgeGraphAdapter(g);
-	Edge e = new Edge("a", "b");
-	assert eg.addEdge(e);
-	assert eg.hasEdge(e);
+	public static void lg_test_removeNode() {
+		Graph g = new ListGraph();
+
+		assert g.addNode("a");
+		assert g.removeNode("a");
+
+		assert g.addNode("b");
+		assert g.removeNode("b");
+
+		assert !g.removeNode("a");
+		assert !g.removeNode("b");
+		assert !g.removeNode("c");
+	}
+
+	public static void lg_test_addEdge() {
+		Graph g = new ListGraph();
+
+		assert g.addEdge("a", "b");
+		assert g.hasEdge("a", "b");
+
+		assert g.addEdge("a", "c");
+		assert g.hasEdge("a", "c");
+
+		assert g.addEdge("b", "c");
+		assert g.hasEdge("b", "c");
+
+		assert !g.hasEdge("b", "d");
+		assert !g.hasEdge("c", "d");
+		assert !g.hasEdge("c", "e");
+	}
+
+	public static void lg_test_removeEdge() {
+		Graph g = new ListGraph();
+
+		assert g.addEdge("a", "b");
+		assert g.removeEdge("a", "b");
+
+		assert g.addEdge("a", "c");
+		assert g.removeEdge("a", "c");
+
+		assert !g.removeEdge("b", "c");
+		assert !g.removeEdge("b", "d");
+	}
+
+	public static void lg_test_nodes() {
+		Graph g = new ListGraph();
+		Set<String> n1 = new HashSet<>();
+
+		n1.add("a");
+		n1.add("b");
+		n1.add("c");
+		n1.add("d");
+		n1.add("e");
+		n1.add("f");
+		n1.add("g");
+
+		assert g.addNode("a");
+		assert g.addNode("b");
+		assert g.addNode("c");
+		assert g.addNode("d");
+		assert g.addNode("e");
+		assert g.addNode("f");
+		assert g.addNode("g");
+
+		Set<String> n2 = new HashSet<>(g.nodes());
+		assert n1.equals(n2);
+
+		assert g.addNode("h");
+		Set<String> n3 = new HashSet<>(g.nodes());
+		assert !n1.equals(n3);
+	}
+
+    /*
+	public static void test_addEdge() {
+		Graph g = new ListGraph();
+		EdgeGraph eg = new EdgeGraphAdapter(g);
+		Edge e = new Edge("a", "b");
+		assert eg.addEdge(e);
+		assert eg.hasEdge(e);
     }
-    
+     */
+
     public static void main(String[] args) {
-	test1();
-	test2();
+		lg_test_addNode();
+		lg_test_removeNode();
+		lg_test_addEdge();
+		lg_test_removeEdge();
+		lg_test_nodes();
+
+
+
+		//test_addEdge();
     }
 
 }
